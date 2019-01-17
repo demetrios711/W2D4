b676d5873d2771d267a88a46765c0dbfb31eb81a
demetrios711 <demetri.sakellaropoulos@gmail.com>
2019-01-17 15:12:00 -0800

    first commit

---
+++ w2d4.rb
@@ -0,0 +1,38 @@
+require 'byebug'
+def my_min_1(arr)
+    arr.each_index do |idx|
+        return arr[idx] if arr.none?{|el| el < arr[idx]}
+    end
+end
+
+def my_min_2(arr)
+    min = arr[0]
+    arr.each do |el|
+        if min > el
+            min = el
+        end
+    end
+    min
+end
+
+def largest_contiguous_subsum1(arr)
+    res = -1.0/0
+    (0...arr.length).each do |i|
+        (i+1...arr.length).each do |j|
+            cur_sum = arr[i..j].inject(:+) 
+            res = cur_sum if res < cur_sum
+        end 
+    end 
+    res
+end
+
+def largest_contiguous_subsum2(arr)
+    #debugger
+    max_cur_sum = arr[0]
+    max_global_sum = arr[0]
+    arr[1..-1].each do |el|
+        max_cur_sum = [el, max_cur_sum + el].max
+        max_global_sum = [max_cur_sum, max_global_sum].max
+    end
+    max_global_sum
+end
